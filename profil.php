8259b83e2b598bb0ed366555c2777ae3047a1844
asmabenhassine-oss <asma.benhassine@edu.isetcom.tn>
2026-02-23 10:11:47 +0100

    Add files via upload

--- profil.php
+++ profil.php
@@ -1,18 +1,25 @@
 <?php
-$nom = "Benhassine";
+$nom = "benhassine";
 $prenom = "Asma";
-$email = "asma.Benhassine@edu.isetcom.tn";
+$email = "Asma.Benhassine@isetcom.tn";
+$age = 20;
+$ville = "tunis";
+$formation = "GTIC";
+
 ?>
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 <head>
-<meta charset="UTF-8">
 <title>Profil utilisateur</title>
 </head>
 <body>
 <h1>Profil utilisateur</h1>
-<p><strong>Nom :</strong> <?= $nom ?></p>
-<p><strong>Prénom :</strong> <?= $prenom ?></p>
-<p><strong>Email :</strong> <?= $email ?></p>
+<p><strong>Nom :</strong> <?php echo $nom; ?></p
+<p><strong>Prénom :</strong> <?php echo $prenom; ?></p>
+<p><strong>Email :</strong> <?php echo $email; ?></p>
+<p><strong>Âge :</strong> <?php echo $age; ?> ans</p>
+<p><strong>ville :</strong> <?php echo $ville; ?></p>
+<p><strong>formation :</strong> <?php echo $formation; ?></p>
+<p>bienvenue <?=$prenom?> dans la formation <?=$formation?></p>
 </body>
 </html>
